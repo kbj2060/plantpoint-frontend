6ccce4fd4ac47a88c09a306d84d0ab767271c2e2
llewyn <kbj2060@naver.com>
2021-03-17 20:56:22 +0900

    [Bug Fix] WS Update

--- src/reference/secret.ts
+++ src/reference/secret.ts
@@ -1,5 +1,5 @@
-export const API = 'http://121.157.207.47:9000';
-export const WS = 'http://121.157.207.47:4000';
+export const API = 'http://localhost:9000';
+export const WS = 'http://localhost:4000';
 export const DB = {
   "host" : "localhost",
   "user" : "root",
