32b4a6a4765746077e7fe80556396c4bb2d3e3b1
llewyn <kbj2060@naver.com>
2021-03-17 17:39:19 +0900

    [Styles] Secret File Update (Camera IP Secret)

--- src/components/CCTV/index.tsx
+++ src/components/CCTV/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import '@styles/components/cctv.scss';
+import {CAM_IP} from "../../reference/secret";
 
 export default function CCTV () {
 	return (
 			<img alt={"cctv"} className='cctv'
-					 src={process.env.PUBLIC_URL + '/sample.png'}
+					 src={CAM_IP}
 					 width='100%' height='100%' /*muted autoPlay={true}*/ />
 	)
 }
